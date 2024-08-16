7f5880aed91b708006e29380ef2bfb9642dc47df
TinasheMuzenda <tmuzendat@gmail.com>
2024-08-16 06:48:05 +0200

    starting about section

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,9 +1,9 @@
 const Page = () => {
   return (
     <>
-      <section className="banner" id="banner">
+      <section className="homeSection" id="banner">
         <div className="container">
-          <div className="bannerContent">
+          <div className="homeContent">
             <h1>Mental Health Association of <span>San Francisco</span></h1>
             <div className="buttons">
               <button>Contact Us</button>
@@ -11,6 +11,9 @@ const Page = () => {
             </div>
           </div>
         </div>
+      </section>
+      <section className="aboutSection" id="about">
+        
       </section>
     </>
   );
